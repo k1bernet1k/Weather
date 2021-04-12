ac995865922c77011c2ab2b5b51c7a840c4d1107
riddik2011@yandex.ru <zaebalblyat1>
2021-04-12 19:06:30 +0300

    2

--- weather/src/App.js
+++ weather/src/App.js
@@ -7,29 +7,92 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      weather: null
+      weather: null,
+      weathet2: null,
+      weather3: null,
+      weather4: null
     }
   }
   componentDidMount() {
     fetch("https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=55&lon=37")
         .then(res => res.json()).then(timeseries=> {
       this.setState({
-        weather: timeseries.properties.timeseries[0].data.instant.details
-
+        weather: timeseries.properties.timeseries
       });
+      // console.log(this.state.weather)
     });
   }
+
   // data.instant.details.air_temperature
   render() {
-    let weather =this.state.weather ? this.state.weather : [{
-      // time :"",
-      // data:"",
-      air_temperature:""
+    // let myweather = this.state.weather ? this.state.weather : [{
+    //   air_temperature: "",
+    //   relative_humidity:"",
+    //   wind_speed: "",
+    //   symbol_code: ""
+    // }];
+    // let cards = myweather.map((bk ,i)=> <Weather key={i}
+    //                                          air_temperature={bk.data.instant.details.air_temperature}
+    //                                          relative_humidity={bk.data.instant.details.relative_humidity}
+    //                                          wind_speed={bk.data.instant.details.wind_speed}
+    //                                          symbol_code={bk.data.next_1_hours.summary.symbol_code}
+    //
+    //     />
+    // )
+
+    let weather = this.state.weather ? this.state.weather[0].data.instant.details : [{
+      air_temperature: "",
+      relative_humidity:"",
+      wind_speed: ""
     }];
-    // console.log(weather[0].data)
+    let cloud = this.state.weather ? this.state.weather[0].data.next_1_hours.summary : [{
+      symbol_code: ""
+    }];
+    let weather2 = this.state.weather ? this.state.weather[5].data.instant.details : [{
+      air_temperature: "",
+      relative_humidity:"",
+      wind_speed: ""
+    }];
+    let cloud2 = this.state.weather2 ? this.state.weather[5].data.next_1_hours.summary : [{
+      symbol_code: ""
+    }];
+    let weather3 = this.state.weather3 ? this.state.weather[11].data.instant.details : [{
+      air_temperature: "",
+      relative_humidity:"",
+      wind_speed: ""
+    }];
+    let cloud3 = this.state.weather ? this.state.weather[11].data.next_1_hours.summary : [{
+      symbol_code: ""
+    }];
+    let weather4 = this.state.weather4 ? this.state.weather[23].data.instant.details : [{
+      air_temperature: "",
+      relative_humidity:"",
+      wind_speed: ""
+    }];
+    let cloud4 = this.state.weather4 ? this.state.weather[23].data.next_1_hours.summary : [{
+      symbol_code: ""
+    }];
+    // console.log(weather)
     return (
         <div className="App">
-          <Weather time={weather.air_temperature}/>
+          <Weather
+              temp={weather.air_temperature}
+              wind={weather.wind_speed}
+              cloud={cloud.symbol_code}
+              humidity={weather.relative_humidity}
+              temp2={weather2.air_temperature}
+              wind2={weather2.wind_speed}
+              cloud2={cloud2.symbol_code}
+              humidit2={weather2.relative_humidity}
+              temp3={weather3.air_temperature}
+              wind3={weather3.wind_speed}
+              cloud3={cloud3.symbol_code}
+              humidity3={weather3.relative_humidity}
+              temp4={weather4.air_temperature}
+              wind4={weather4.wind_speed}
+              cloud4={cloud4.symbol_code}
+              humidity4={weather4.relative_humidity}
+          />
         </div>
     );
   }
--- weather/src/component/Weather.js
+++ weather/src/component/Weather.js
@@ -1,9 +1,49 @@
 import {Component} from "react";
+import "./Weather.css"
 export default class Weather extends Component{
     render(){
+        const rus = {
+            partlycloudy_day: "Переменная облачность",
+            partlycloudy_night: "Переменная облачность",
+            fair_night: "Безоблачно",
+            clearsky_night: "Безоблачно",
+            cloudy: "Облачно",
+            fair_day: "Безоблачно",
+            clearsky_day: "Безоблачно",
+        }
+
         return <div className="weather">
-            <h2>Погода в москве</h2>
-            <p>Температура {this.props.time}</p>
+            <h2>Погода в Москве</h2>
+            <div className="time">
+            <div>
+                <h3>Сейчас</h3>
+                <p>Температура {this.props.temp}</p>
+                <p>Скорость ветра {this.props.wind}</p>
+                <p>Облачность: {rus[this.props.cloud]}</p>
+                <p>Влажность: {this.props.humidity}</p>
+            </div>
+            <div>
+                <h3>Через 6 часов</h3>
+                <p>Температура {this.props.temp2}</p>
+                <p>Скорость ветра {this.props.wind2}</p>
+                <p>Облачность: {rus[this.props.cloud2]}</p>
+                <p>Влажность: {this.props.humidity}2</p>
+            </div>
+            <div>
+                <h3>Через 12 часов</h3>
+                <p>Температура {this.props.temp3}</p>
+                <p>Скорость ветра {this.props.wind3}</p>
+                <p>Облачность: {rus[this.props.cloud3]}</p>
+                <p>Влажность: {this.props.humidity3}</p>
+            </div>
+            <div>
+                <h3>Через 24 часа</h3>
+                <p>Температура {this.props.temp4}</p>
+                <p>Скорость ветра {this.props.wind4}</p>
+                <p>Облачность: {rus[this.props.cloud4]}</p>
+                <p>Влажность: {this.props.humidity4}</p>
+            </div>
+            </div>
         </div>
     }
 }
